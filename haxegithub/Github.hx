15038e7838be69356399abe920f3421d87d19c9b
GuineaPigUuhh <93527295+GuineaPigUuhh@users.noreply.github.com>
2024-02-29 14:45:40 -0300

    User Typedef and Better Functions

--- haxegithub/Github.hx
+++ haxegithub/Github.hx
@@ -2,6 +2,7 @@ package haxegithub;
 
 import haxe.Json;
 import haxe.Http;
+import haxegithub.types.*;
 
 /**
  * Haxe Github API
@@ -13,96 +14,78 @@ class Github {
      * get User JSON
      * @param user 
      */
-    public static function getUser(user:String) {
-        if(user != '')
-            return githubData('https://api.github.com/users/${user}');
-        return '';
-    }
+    public static function getUser(user:String) 
+        return githubData('https://api.github.com/users/${user}');
+    
 
     /**
-     * get User Repo
+     * get User Repository JSON
      * @param user 
      * @param repo 
      */
-    public static function getRepo(user:String, repo:String) {
-        if(user != '' && repo != '')
-            return githubData('https://api.github.com/repos/${user}/${repo}');
-        return '';
-    }
+    public static function getRepo(user:String, repo:String) 
+        return githubParse('https://api.github.com/repos/${user}/${repo}');
+    
 
     /**
-     * search a User
+     * search a User and get the JSON
      * @param user 
      */
-    public static function searchUser(user:String) {
-        if(user != '')
-            return githubData('https://api.github.com/search/users?q=${user}');
-        return '';
-    }
+    public static function searchUser(user:String) 
+        return githubParse('https://api.github.com/search/users?q=${user}');
 
     /**
-     * search a Repository
+     * search a Repository and get the JSON
      * @param repo 
      */
-    public static function searchRepo(repo:String) {
-        if(repo != '')
-            return githubData('https://api.github.com/search/repositories?q=${repo}');
-        return '';
-    }
+    public static function searchRepo(repo:String) 
+        return githubParse('https://api.github.com/search/repositories?q=${repo}');
 
     /**
-     * search a Topic
+     * search a Topic and get the JSON
      * @param topic 
      */
-    public static function searchTopic(topic:String) {
-        if(topic != '')
-            return githubData('https://api.github.com/search/topics?q=${topic}');
-        return '';
-    }
+    public static function searchTopic(topic:String) 
+        return githubParse('https://api.github.com/search/topics?q=${topic}');
 
     /**
-     * search a Issue
+     * search a Issue and get the JSON
      * @param issue 
      */
-    public static function searchIssue(issue:String) {
-        if(issue != '')
-            return githubData('https://api.github.com/search/issues?q=$issue');
-        return '';
-    }
+    public static function searchIssue(issue:String) 
+        return githubParse('https://api.github.com/search/issues?q=$issue');
 
     /**
-     * search a Code
+     * search a Code and get the JSON
      * @param code 
      */
-    public static function searchCode(code:String) {
-        if(code != '')
-            return githubData('https://api.github.com/search/code?q=$code');
-        return '';
-    }
+    public static function searchCode(code:String) 
+        return githubParse('https://api.github.com/search/code?q=$code');
 
     /**
-     * search Commit
+     * search Commit and get the JSON
      * @param commit 
      */
-    public static function searchCommit(commit:String) {
-        if(commit != '')
-            return githubData('https://api.github.com/search/commits?q=$commit');
-        return '';
-    }
+    public static function searchCommit(commit:String) 
+        return githubParse('https://api.github.com/search/commits?q=$commit');
 
     /**
-     * search Label
+     * search Label and get the JSON
      * @param label 
      * @param id 
      */
-    public static function searchLabel(label:String, id:String) {
-        if(label != '')
-            return githubData('https://api.github.com/search/labels?q=${label}&repository_id=${id}');
-        return '';
-    }
+    public static function searchLabel(label:String, id:String)
+        return githubParse('https://api.github.com/search/labels?q=${label}&repository_id=${id}');
+
+    /**
+     * easy Parse Github JSON
+     * @param url 
+     */
+    public static function githubParse(url:String)
+        return Json.parse(githubData(url));
     
     /**
-     * is used to give a request to github and returns a string
+     * is used to give a request to github and returns a JSON string
      * @param url 
      */
     public static function githubData(url:String) {
