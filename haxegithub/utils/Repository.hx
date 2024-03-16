49d99725a087c32432dd053e59e7020190cf65d2
GuineaPigUuhh <93527295+GuineaPigUuhh@users.noreply.github.com>
2024-03-15 22:04:15 -0300

    Fix

--- haxegithub/utils/Repository.hx
+++ haxegithub/utils/Repository.hx
@@ -1,23 +1,23 @@
-package haxegithub.classes;
+package haxegithub.utils;
 
 import haxegithub.Request;
 
 class Repository {
-    /**
-     * Return the Repository JSON
-     * @param user 
-     * @param repo 
-     */
-    public static function get(user:String, repo:String) {
-        return Request.easyparse('repos/$user/$repo');
-    }
+	/**
+	 * Return the Repository JSON
+	 * @param user 
+	 * @param repo 
+	 */
+	public static function get(user:String, repo:String) {
+		return Request.easyparse('repos/$user/$repo');
+	}
 
-    /**
-     * get Contributors from a Repository
-     * @param user 
-     * @param repo 
-     */
-    public static function getContributors(user:String, repo:String) {
-        return Request.easyparse('repos/$user/$repo/collaborators');
-    }
+	/**
+	 * get Contributors from a Repository
+	 * @param user 
+	 * @param repo 
+	 */
+	public static function getContributors(user:String, repo:String) {
+		return Request.easyparse('repos/$user/$repo/collaborators');
+	}
 }
--- haxegithub/utils/User.hx
+++ haxegithub/utils/User.hx
@@ -1,53 +1,53 @@
-package haxegithub.classes;
+package haxegithub.utils;
 
 import haxegithub.Request;
 
 class User {
-    /**
-     * Return the User JSON
-     * @param user 
-     */
-    public static function get(user:String) {
-        return Request.easyparse('users/$user');
-    }
+	/**
+	 * Return the User JSON
+	 * @param user 
+	 */
+	public static function get(user:String) {
+		return Request.easyparse('users/$user');
+	}
 
-    /**
-     * Return the User Followers JSON
-     * @param user 
-     */
-    public static function getFollowers(user:String) {
-        return Request.easyparse('users/$user/followers');
-    }
+	/**
+	 * Return the User Followers JSON
+	 * @param user 
+	 */
+	public static function getFollowers(user:String) {
+		return Request.easyparse('users/$user/followers');
+	}
 
-    /**
-     * Return the User Following Json
-     * @param user 
-     */
-    public static function getFollowing(user:String) {
-        return Request.easyparse('users/$user/following');
-    }
+	/**
+	 * Return the User Following Json
+	 * @param user 
+	 */
+	public static function getFollowing(user:String) {
+		return Request.easyparse('users/$user/following');
+	}
 
-    /**
-     * Return the Repositorys from the User
-     * @param user 
-     */
-    public static function getRepositorys(user:String) {
-        return Request.easyparse('users/$user/repos');
-    }
+	/**
+	 * Return the Repositorys from the User
+	 * @param user 
+	 */
+	public static function getRepositorys(user:String) {
+		return Request.easyparse('users/$user/repos');
+	}
 
-    /**
-     * Return the Gits from the user
-     * @param user 
-     */
-    public static function getGists(user:String) {
-        return Request.easyparse('users/$user/gists');
-    }
+	/**
+	 * Return the Gits from the user
+	 * @param user 
+	 */
+	public static function getGists(user:String) {
+		return Request.easyparse('users/$user/gists');
+	}
 
-    /**
-     * Return the Starred Repositorys from the User
-     * @param user 
-     */
-    public static function getStarred(user:String) {
-        return Request.easyparse('users/$user/starred');
-    }
+	/**
+	 * Return the Starred Repositorys from the User
+	 * @param user 
+	 */
+	public static function getStarred(user:String) {
+		return Request.easyparse('users/$user/starred');
+	}
 }
